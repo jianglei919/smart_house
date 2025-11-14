83c95c7f9e24f15b7e4addc9811492b07addddde
lankun <lankun1234@163.com>
2025-11-13 20:39:58 -0500

    删除多余注释

--- src/ThreeHelper/types/img.d.ts
+++ src/ThreeHelper/types/img.d.ts
@@ -1,12 +1,5 @@
 /*
  * @Author: smartHome
- * @Date: 2022-10-23 16:19:28
- * @LastEditors: smartHome
- * @LastEditTime: 2022-10-23 16:19:30
- * @Description:
- */
-/*
- * @Author: smartHome
  * @Date: 2025-11-04 13:06:33
  * @LastEditors: smartHome
  * @LastEditTime: 2025-11-04 13:06:34
